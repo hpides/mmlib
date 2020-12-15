4a505a1cda04488fb05f02fb961c8e04cb46fd95
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2020-12-15 22:53:54 +0100

    generate archives script

--- generate-archives.sh
+++ generate-archives.sh
@@ -3,6 +3,12 @@
 # operate in the current dir
 cd "$(dirname "$0")"
 
+VENV=./venv
+
+python3 -m venv $VENV
+source $VENV/bin/activate
+python3 -m pip install --upgrade pip
+
 # install requirements
 python3 -m pip install --upgrade setuptools wheel
 python3 -m pip install -r requirements-tests.txt
