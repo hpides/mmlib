4880ceadb6a170cdc8c59b281ea8060dd3ab5cfa
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2020-12-30 17:51:20 +0100

    deterministic_backward_compare

--- examples/probe_example.py
+++ examples/probe_example.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torchvision import models
 
+from mmlib.deterministic import set_deterministic
 from mmlib.model_equals import imagenet_input
 from mmlib.probe import ProbeInfo, probe_inference, print_summary, compare_summaries, probe_training
 
@@ -63,9 +64,40 @@ def backward_compare():
     compare_summaries(summary1, summary2, compare, common=common)
 
 
+def deterministic_backward_compare():
+    model1 = models.vgg19(pretrained=True)
+    model2 = models.vgg19(pretrained=True)
+
+    dummy_input = imagenet_input()
+    loss_func = nn.CrossEntropyLoss()
+    dummy_target = torch.tensor([1])
+
+    # two optimizer objects because they have internal state, e.g. update their momentum
+    optimizer1 = torch.optim.SGD(model1.parameters(), 1e-4, momentum=0.9, weight_decay=1e-4)
+    optimizer2 = torch.optim.SGD(model1.parameters(), 1e-4, momentum=0.9, weight_decay=1e-4)
+
+    # in this case the important bit is that the seed are set before execution
+    set_deterministic()
+    summary1 = probe_training(model1, dummy_input, optimizer1, loss_func, dummy_target)
+    # reset seeds
+    set_deterministic()
+    summary2 = probe_training(model2, dummy_input, optimizer2, loss_func, dummy_target)
+
+    # fields that should for sure be the same
+    common = [ProbeInfo.LAYER]
+
+    # fields where we might expect different values
+    compare = [ProbeInfo.INPUT_HASH, ProbeInfo.OUTPUT_HASH, ProbeInfo.GRAD_INPUT_HASH, ProbeInfo.GRAD_OUTPUT_HASH]
+
+    # print the comparison of summary1 and summary2
+    compare_summaries(summary1, summary2, compare, common=common)
+
+
 if __name__ == '__main__':
     summary()
     print('\n\n\n')
     forward_compare()
     print('\n\n\n')
     backward_compare()
+    print('\n\n\n')
+    deterministic_backward_compare()
