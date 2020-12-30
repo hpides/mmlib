94ce53bf8c3f93db177056aca33cbb5e0eee4076
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2020-12-30 12:18:14 +0100

    training forward

--- mmlib/probe.py
+++ mmlib/probe.py
@@ -38,9 +38,9 @@ def probe_training(model, inp, optimizer, loss_func, target, device="cuda"):
 
 def probe_reproducibility(model, inp, mode, optimizer=None, loss_func=None, target=None, device="cuda"):
     if mode == ProbeMode.TRAINING:
-        assert optimizer, 'for training mode a optimizer is needed'
-        assert loss_func, 'for training mode a loss_func is needed'
-        assert target, 'for training mode a target is needed'
+        assert optimizer is not None, 'for training mode a optimizer is needed'
+        assert loss_func is not None, 'for training mode a loss_func is needed'
+        assert target is not None, 'for training mode a target is needed'
 
     def register_forward_hook(module, ):
 
@@ -99,6 +99,7 @@ def compare_summaries(summary1, summary2, compare, common=None):
     assert summary1.keys() == summary2.keys(), 'summary keys do not match'
     assert len(compare) > 0, 'you have to compare at least one attribute'
 
+    # make sure Layer info is included
     if not common:
         common = [ProbeInfo.LAYER]
     if ProbeInfo.LAYER not in common:
@@ -208,9 +209,22 @@ if __name__ == '__main__':
         print('Model: {}'.format(mod.__name__))
         output_info = [ProbeInfo.LAYER, ProbeInfo.INPUT_SHAPE, ProbeInfo.INPUT_HASH, ProbeInfo.OUTPUT_SHAPE,
                        ProbeInfo.OUTPUT_HASH]
-        summary1 = probe_inference(model1, tensor1)
-        summary2 = probe_inference(model2, tensor1)
-        print_summary(summary1, output_info)
-        compare_summaries(summary1, summary2, [ProbeInfo.INPUT_HASH, ProbeInfo.OUTPUT_HASH],
-                          common=[ProbeInfo.LAYER, ProbeInfo.INPUT_SHAPE])
+        # summary1 = probe_inference(model1, tensor1)
+        # summary2 = probe_inference(model2, tensor1)
+        # print_summary(summary1, output_info)
+        # compare_summaries(summary1, summary2, [ProbeInfo.INPUT_HASH, ProbeInfo.OUTPUT_HASH],
+        #                   common=[ProbeInfo.LAYER, ProbeInfo.INPUT_SHAPE])
         print('\n\n')
+
+        loss_func = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model1.parameters(), 1e-4,
+                                    momentum=0.9,
+                                    weight_decay=1e-4)
+        dummy_target = torch.tensor([1])
+
+        model1 = mod(pretrained=True)
+        model2 = mod(pretrained=True)
+        sum_train1 = probe_training(model1, inp=tensor1, optimizer=optimizer, loss_func=loss_func, target=dummy_target)
+        sum_train2 = probe_training(model2, inp=tensor1, optimizer=optimizer, loss_func=loss_func, target=dummy_target)
+        compare_summaries(sum_train1, sum_train2, [ProbeInfo.INPUT_HASH, ProbeInfo.OUTPUT_HASH],
+                          common=[ProbeInfo.LAYER, ProbeInfo.INPUT_SHAPE])
