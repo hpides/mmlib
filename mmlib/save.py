6d3346f47c7ed28442c34be89b89813167363f27
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-01-19 16:47:33 +0000

    develop and test basic functionality to insert json to mongo db and retrieve by id

--- mmlib/save.py
+++ mmlib/save.py
@@ -1,6 +1,66 @@
-def save_model(model):
-    pass
+from pymongo import MongoClient
 
 
-def saved_models():
-    pass
+class SaveService:
+    def __init__(self, host='127.0.0.1'):
+        self.mongo_service = MongoService(host)
+
+    def model_to_dict(self, model):
+        pass
+
+    def save_model(self, model):
+        model_dict = self.model_to_dict(model)
+        id = self.mongo_service.save_dict(model_dict)
+        return id
+
+    def saved_models(self):
+        pass
+
+
+MODELS = 'models'
+MMLIB = 'mmlib'
+ID = '_id'
+
+
+class MongoService(object):
+    def __init__(self, host):
+        self._mongo_client = MongoClient(host)
+        self._db_name = MMLIB
+        self._collection_name = MODELS
+        # close connection for now, for new requests there will be a reconnect
+        self._mongo_client.close()
+
+    def save_dict(self, insert_dict):
+        """
+        Saves a python dictionary in the underlying mongoDB.
+        :param insert_dict: The dictionary to save.
+        :return: The id that was automatically assigned by mongoDB.
+        """
+        collection = self._get_collection()
+        feedback = collection.insert_one(insert_dict)
+
+        return feedback.inserted_id
+
+    def get_ids(self):
+        """
+        Gets all ids that can be found in the used mongoDB collection
+        :return: The list of ids.
+        """
+        collection = self._get_collection()
+
+        return collection.find({}).distinct(ID)
+
+    def get_model_dict(self, model_id):
+        """
+        Retrieves the model dict identified by its mongoDB id.
+        :param model_id: The mongoDB id to find the model dict.
+        :return: The retrieved model dict.
+        """
+        collection = self._get_collection()
+
+        return collection.find({ID: model_id})[0]
+
+    def _get_collection(self):
+        db = self._mongo_client[self._db_name]
+        collection = db[self._collection_name]
+        return collection
---
+++ tests/test_save.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+
+from mmlib.save import MongoService
+
+MONGO_CONTAINER_NAME = 'mongo-test'
+
+
+class TestProbe(unittest.TestCase):
+
+    @classmethod
+    def setUp(self) -> None:
+        os.system('docker run --rm --name %s -it -p 27017:27017 -d  mongo:latest' % MONGO_CONTAINER_NAME)
+        self.mongo_service = MongoService('127.0.0.1')
+
+    def tearDown(self) -> None:
+        os.system('docker kill %s' % MONGO_CONTAINER_NAME)
+
+    def test_save_json(self):
+        test_dict = {'test': 'test'}
+        model_id = self.mongo_service.save_dict(test_dict)
+        expected_dict = test_dict
+        expected_dict['_id'] = model_id
+
+        num_entries = len(self.mongo_service.get_ids())
+        retrieve = self.mongo_service.get_model_dict(model_id=model_id)
+
+        self.assertEqual(1, num_entries)
+        self.assertEqual(expected_dict, retrieve)
