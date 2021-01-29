3b22b732dea8b3323c9cf4a5eae919378429568d
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-01-29 14:52:41 +0100

    include util in library

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/slin96/mmlib",
-    packages=['mmlib'],
+    packages=['mmlib', 'util'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
