56d7ed39ce77aaf439f950bc7c256ca2981847b3
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-02-22 09:54:08 +0100

    dataset constructor

--- schema/dataset.py
+++ schema/dataset.py
@@ -8,6 +8,21 @@ RAW_DATA_SIZE = 'raw_data_size'
 
 
 class Dataset(SchemaObj):
+
+    def __init__(self, d_id: str, dataset_code: str, generate_call: str, raw_data: str, raw_data_size: str):
+        self.d_id = d_id
+        self.dataset_code = dataset_code
+        self.generate_call = generate_call
+        self.raw_data = raw_data
+        self.raw_data_size = raw_data_size
+        self.type_mapping = {
+            ID: SchemaObjType.STRING,
+            DATASET_CODE: SchemaObjType.FILE,
+            GENERATE_CALL: SchemaObjType.STRING,
+            RAW_DATA: SchemaObjType.FILE,
+            RAW_DATA_SIZE: SchemaObjType.STRING,
+        }
+
     def to_dict(self) -> dict:
         pass
 
