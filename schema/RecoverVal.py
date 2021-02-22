14930ac69a37299b5b8f0e08ff95aa906535af2f
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-02-22 09:17:38 +0100

    recover-val constructor

--- schema/RecoverVal.py
+++ schema/RecoverVal.py
@@ -2,6 +2,13 @@ from schema.schema_obj import SchemaObj, SchemaObjType
 
 
 class RecoverVal(SchemaObj):
+
+    def __init__(self, r_id: str, weights_hash: str, inference_hash: str, inference_data: str):
+        self.r_id = r_id
+        self.weights_hash = weights_hash
+        self.inference_hash = inference_hash
+        self.inference_data = inference_data
+
     def to_dict(self) -> dict:
         pass
 
