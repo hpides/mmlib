40167b51d36990226ac05a0fc960f96eb68b0fd1
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-03-07 12:23:20 +0100

    comments

--- schema/save_info_builder.py
+++ schema/save_info_builder.py
@@ -11,24 +11,23 @@ class FullModelSafeInfoBuilder:
         self.code_name = None
         self.recover_val = False
         self.dummy_input_shape = None
-    """TODO use comments
-           Saves a model together with the given metadata.
-           :param model: The actual model to save as an instance of torch.nn.Module.
-           :param code: The path to the code of the model (is needed for recover process).
-           :param code_name: The name of the model, i.e. the model constructor (is needed for recover process).
-           :param recover_val: Indicates if along with the model itself also information is stored to later validate that
-           restoring the model lead to the exact same model. It is checked by comparing the model weights and the inference
-           result on dummy input. If this flag is true, a dummy_input_shape has to be provided.
-           :param dummy_input_shape: The shape of the dummy input that should be used to produce an inference result.
-           :return: Returns the id that was used to store the model.
-           """
 
     def add_model_info(self, model: torch.nn.Module, code: str, code_name: str):
+        """
+        Adds the general model information
+        :param model: The actual model to save as an instance of torch.nn.Module.
+        :param code: The path to the code of the model (is needed for recover process).
+        :param code_name: The name of the model, i.e. the model constructor (is needed for recover process).
+        """
         self.model = model
         self.code = code
         self.code_name = code_name
 
     def add_recover_val(self, dummy_input_shape: [int] = None):
+        """
+        Indicates that recover validation info should be saved and adds the required info.
+        :param dummy_input_shape: The shape of the dummy input that should be used to produce an inference result.
+        """
         self.recover_val = True
         self.dummy_input_shape = dummy_input_shape
 
