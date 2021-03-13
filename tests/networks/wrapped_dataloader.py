c704f5923966f671a5068fe776b0cb329354fb99
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-03-13 17:50:09 +0000

    reformat

--- tests/networks/wrapped_dataloader.py
+++ tests/networks/wrapped_dataloader.py
@@ -3,11 +3,11 @@ from torch.utils.data import DataLoader
 
 from schema.restorable_object import RestorableObjectWrapper
 
+
 class WrappedDataLoader(RestorableObjectWrapper):
 
     def _save_instance_state(self):
         dataloader: torch.utils.data.DataLoader = None
-
 
     def _restore_instance_from_state(self):
         pass
