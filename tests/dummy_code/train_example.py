681dd0a13dd8cffef2c840fb3f80d37e78b7b4cf
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-03-16 12:37:43 +0000

    train example works

--- tests/dummy_code/train_example.py
+++ tests/dummy_code/train_example.py
@@ -3,12 +3,14 @@ import tempfile
 
 import torch
 
+from mmlib.equal import model_equal
 from mmlib.persistence import FileSystemPersistenceService, MongoDictPersistenceService
 from schema.restorable_object import RestorableObjectWrapper
 from tests.dummy_code.resnet_train import OptimizerWrapper, ResnetTrainService, ResnetTrainWrapper
 from tests.networks.mynets.resnet18 import resnet18
 from tests.test_dict_persistence import MONGO_CONTAINER_NAME
 from tests.test_save import CONFIG
+from util.dummy_data import imagenet_input
 
 if __name__ == '__main__':
     # set path for config file
@@ -22,7 +24,7 @@ if __name__ == '__main__':
         os.system('docker run --rm --name %s -it -p 27017:27017 -d  mongo:4.4.3 ' % MONGO_CONTAINER_NAME)
         dict_ps = MongoDictPersistenceService()
 
-        model = resnet18()
+        model = resnet18(pretrained=True)
 
         # prepare state dict for ResnetTrainService
         # TODO think about how to get rid of magic strings
@@ -74,6 +76,10 @@ if __name__ == '__main__':
         # train with 2 batches
         resnet_ts.train(model, number_batches=2)
 
+        # save the model for later comparison
+        resnet_weights_path = os.path.join(tmp_path, 'res18_model_weights')
+        torch.save(model.state_dict(), resnet_weights_path)
+
         # persist the train service
         ts_wrapper_id = ts_wrapper.persist(file_ps, dict_ps)
 
@@ -84,3 +90,23 @@ if __name__ == '__main__':
 
         # train with another 2 batches
         resnet_ts_new.train(model, number_batches=2)
+
+        # restore model from before and restore ResnetTrainWrapper to compare results
+        # we expect the resulting models to be the same
+        second_model = resnet18()
+        second_model.load_state_dict(torch.load(resnet_weights_path))
+
+        # load the same wrapper back to continue second (identical) training
+        ts_wrapper_new = ResnetTrainWrapper.load(ts_wrapper_id, file_ps, dict_ps, tmp_path)
+        ts_wrapper_new.restore_instance(file_ps, dict_ps, tmp_path)
+        resnet_ts_new: ResnetTrainService = ts_wrapper_new.instance
+
+        # train with another 2 batches
+        resnet_ts_new.train(second_model, number_batches=2)
+
+        # starting from the same intermediate model the both restored versions of the resnet train service
+        # should train the models in the same way
+        # TODO check for other models and on GPU where we have to set "set.deterministic()"
+        eq = model_equal(model, second_model, imagenet_input)
+
+        print(eq)
