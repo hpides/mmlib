5a2f06d9beed7de1440c699b8b282120afde85ca
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-04-06 12:34:20 +0100

    removed NVIDIA specific fields

--- schema/environment.py
+++ schema/environment.py
@@ -8,17 +8,14 @@ ENVIRONMENT = 'environment'
 class Environment(SchemaObj):
 
     def __init__(self, store_id: str = None, python_version: str = None, pytorch_version: str = None,
-                 processor: str = None, gpu_types: [str] = None, cuda_version: str = None, cudnn_version: str = None,
-                 driver_version: str = None, pytorch_info: str = None, python_platform_info: dict = None,
+                 processor_info: str = None, gpu_types: [str] = None, pytorch_info: str = None,
+                 python_platform_info: dict = None,
                  pip_freeze: list = None):
         super().__init__(store_id)
         self.python_version = python_version
         self.pytorch_version = pytorch_version
-        self.processor = processor
+        self.processor_info = processor_info
         self.gpu_types = gpu_types
-        self.cuda_version = cuda_version
-        self.cudnn_version = cudnn_version
-        self.driver_version = driver_version
         self.pytorch_info = pytorch_info
         self.python_platform_info = python_platform_info
         self.pip_freeze = pip_freeze
