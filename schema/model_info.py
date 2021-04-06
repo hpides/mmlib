ee2683094c91c58873e5f8d519d4430ca13c8386
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-04-06 10:35:33 +0100

    remove train and inference info from ModelInfo

--- schema/model_info.py
+++ schema/model_info.py
@@ -1,9 +1,7 @@
 from mmlib.persistence import AbstractFilePersistenceService, AbstractDictPersistenceService
-from schema.inference_info import InferenceInfo
 from schema.recover_info import AbstractRecoverInfo, FullModelRecoverInfo, ProvenanceRecoverInfo
 from schema.schema_obj import SchemaObj
 from schema.store_type import ModelStoreType
-from schema.train_info import TrainInfo
 
 STORE_TYPE = 'store_type'
 RECOVER_INFO_ID = 'recover_info_id'
@@ -17,14 +15,11 @@ MODEL_INFO = 'model_info'
 class ModelInfo(SchemaObj):
 
     def __init__(self, store_type: ModelStoreType = None, recover_info: AbstractRecoverInfo = None,
-                 store_id: str = None, derived_from_id: str = None, inference_info: InferenceInfo = None,
-                 train_info: TrainInfo = None):
+                 store_id: str = None, derived_from_id: str = None):
         super().__init__(store_id)
         self.store_type = store_type
         self.recover_info = recover_info
         self.derived_from = derived_from_id
-        self.inference_info = inference_info
-        self.train_info = train_info
 
     def _persist_class_specific_fields(self, dict_representation, file_pers_service, dict_pers_service):
 
@@ -39,12 +34,6 @@ class ModelInfo(SchemaObj):
         # add optional fields if set
         if self.derived_from:
             dict_representation[DERIVED_FROM] = self.derived_from
-        if self.inference_info:
-            inference_info_id = self.inference_info.persist(file_pers_service, dict_pers_service)
-            dict_representation[INFERENCE_INFO_ID] = inference_info_id
-        if self.train_info:
-            train_info_id = self.train_info.persist(file_pers_service, dict_pers_service)
-            dict_representation[TRAIN_INFO_ID] = train_info_id
 
     def load_all_fields(self, file_pers_service: AbstractFilePersistenceService,
                         dict_pers_service: AbstractDictPersistenceService, restore_root: str,
@@ -64,17 +53,6 @@ class ModelInfo(SchemaObj):
         if not self.derived_from:
             self.derived_from = _recover_derived_from(restored_dict)
 
-        # NOTE: other fields not implemented yet
-
-        # Note not implemented yet
-        # if INFERENCE_INFO_ID in restored_dict:
-        #     inference_info_id = restored_dict[INFERENCE_INFO_ID]
-        #     inference_info = InferenceInfo.load(inference_info_id, file_pers_service, dict_pers_service, restore_root)
-        #
-        # if TRAIN_INFO_ID in restored_dict:
-        #     train_info_id = restored_dict[TRAIN_INFO_ID]
-        #     train_info = TrainInfo.load(train_info_id, file_pers_service, dict_pers_service, restore_root)
-
     def size_in_bytes(self, file_pers_service: AbstractFilePersistenceService,
                       dict_pers_service: AbstractDictPersistenceService) -> int:
         result = 0
@@ -85,10 +63,6 @@ class ModelInfo(SchemaObj):
         # size of all referenced files/objects
         # for now we leave out the size of the base model, we might have to implement this later
         result += self.recover_info.size_in_bytes(file_pers_service, dict_pers_service)
-        if self.inference_info:
-            result += self.inference_info.size_in_bytes(file_pers_service, dict_pers_service)
-        if self.train_info:
-            result += self.train_info.size_in_bytes(file_pers_service, dict_pers_service)
 
         return result
 
