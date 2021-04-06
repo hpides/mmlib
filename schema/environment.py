0a246eb87656a5de2b139831965c83a011e0b23d
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-04-06 14:27:38 +0100

    added missing functions

--- schema/environment.py
+++ schema/environment.py
@@ -1,6 +1,5 @@
+from mmlib.persistence import FilePersistenceService, DictPersistenceService
 from schema.schema_obj import SchemaObj
-
-ENVIRONMENT_DICT = 'environment_dict'
 
 ENVIRONMENT = 'environment'
 
@@ -9,8 +8,7 @@ class Environment(SchemaObj):
 
     def __init__(self, store_id: str = None, python_version: str = None, pytorch_version: str = None,
                  processor_info: str = None, gpu_types: [str] = None, pytorch_info: str = None,
-                 python_platform_info: dict = None,
-                 pip_freeze: list = None):
+                 python_platform_info: dict = None, pip_freeze: list = None):
         super().__init__(store_id)
         self.python_version = python_version
         self.pytorch_version = pytorch_version
@@ -19,6 +17,17 @@ class Environment(SchemaObj):
         self.pytorch_info = pytorch_info
         self.python_platform_info = python_platform_info
         self.pip_freeze = pip_freeze
+
+    def load_all_fields(self, file_pers_service: FilePersistenceService, dict_pers_service: DictPersistenceService,
+                        restore_root: str, load_recursive: bool = True, load_files: bool = True):
+        pass
+
+    def size_in_bytes(self, file_pers_service: FilePersistenceService,
+                      dict_pers_service: DictPersistenceService) -> int:
+        raise NotImplementedError
+
+    def _persist_class_specific_fields(self, dict_representation, file_pers_service, dict_pers_service):
+        pass
 
     def _representation_type(self) -> str:
         return ENVIRONMENT
