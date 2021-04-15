e71d6e9ec1d76d6529640339e05a59f9fa5591c7
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-04-15 17:58:59 +0200

    introduced WeightsUpdateRecoverInfo

--- schema/recover_info.py
+++ schema/recover_info.py
@@ -120,6 +120,23 @@ DATASET = 'dataset'
 TRAIN_INFO = 'train_info'
 
 
+class WeightsUpdateRecoverInfo(SchemaObj):
+
+    def load_all_fields(self, file_pers_service: FilePersistenceService, dict_pers_service: DictPersistenceService,
+                        restore_root: str, load_recursive: bool = True, load_files: bool = True):
+        pass
+
+    def size_in_bytes(self, file_pers_service: FilePersistenceService,
+                      dict_pers_service: DictPersistenceService) -> int:
+        pass
+
+    def _representation_type(self) -> str:
+        pass
+
+    def _persist_class_specific_fields(self, dict_representation, file_pers_service, dict_pers_service):
+        pass
+
+
 class ProvenanceRecoverInfo(AbstractModelCodeRecoverInfo):
 
     def __init__(self, dataset: Dataset = None, model_code=None, model_class_name: str = None,
