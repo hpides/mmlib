007f442d0c8fb159b2eb144829a0dce7edffb521
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-04-16 10:58:47 +0200

    first version of TestWeightUpdateSaveService

---
+++ tests/test_weight_update_save_service.py
@@ -0,0 +1,8 @@
+from mmlib.save import BaselineSaveService, WeightUpdateSaveService
+from tests.test_baseline_save_servcie import TestBaselineSaveService
+
+
+class TestWeightUpdateSaveService(TestBaselineSaveService):
+
+    def init_save_service(self, dict_pers_service, file_pers_service):
+        self.save_service = WeightUpdateSaveService(file_pers_service, dict_pers_service)
