cb412d50974e527b3f6c78bf28c07429f869a385
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-04-18 21:39:20 +0200

    added new testcase for baseline testing

--- tests/save/test_baseline_save_servcie.py
+++ tests/save/test_baseline_save_servcie.py
@@ -132,3 +132,35 @@ class TestBaselineSaveService(unittest.TestCase):
         restored_model_info = self.save_service.recover_model(derived_model_id)
 
         self.assertTrue(model_equal(derived_model, restored_model_info.model, imagenet_input))
+
+    def test_save_restore_multiple_derived_models(self):
+        code_file = NETWORK_CODE_TEMPLATE.format(RESNET_18)
+        initial_model = resnet18()
+
+        # save initial model
+        save_info_builder = ModelSaveInfoBuilder()
+        save_info_builder.add_model_info(initial_model, code_file)
+        save_info = save_info_builder.build()
+        initial_model_id = self.save_service.save_model(save_info)
+
+        # save derived model
+        derived_model = resnet18(pretrained=True)
+        save_info_builder = ModelSaveInfoBuilder()
+        save_info_builder.add_model_info(derived_model, code_file, base_model_id=initial_model_id)
+        save_info = save_info_builder.build()
+        derived_model_id = self.save_service.save_model(save_info)
+
+        restored_model_info = self.save_service.recover_model(derived_model_id)
+
+        self.assertTrue(model_equal(derived_model, restored_model_info.model, imagenet_input))
+
+        # save derived model
+        derived_model_2 = resnet18()
+        save_info_builder = ModelSaveInfoBuilder()
+        save_info_builder.add_model_info(derived_model_2, code_file, base_model_id=derived_model_id)
+        save_info = save_info_builder.build()
+        derived_model_id_2 = self.save_service.save_model(save_info)
+
+        restored_model_info_2 = self.save_service.recover_model(derived_model_id_2)
+
+        self.assertTrue(model_equal(derived_model_2, restored_model_info_2.model, imagenet_input))
