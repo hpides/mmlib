c5b2d1b7a20ae90abf13422019f08d34e8be6e6e
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-05-06 23:28:50 +0200

    WeightDictMerkleTreeNode

--- util/weight_dict_merkle_tree.py
+++ util/weight_dict_merkle_tree.py
@@ -1,6 +1,23 @@
 from typing import Dict
 
+import torch
 from torch import Tensor
+
+from util.hash import tensor_hash
+
+
+class WeightDictMerkleTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    @property
+    def hash_value(self):
+        if isinstance(str, self.value):
+            return self.value
+        elif isinstance(torch.tensor, self.value):
+            return tensor_hash(self.value)
 
 
 class WeightDictMerkleTree:
