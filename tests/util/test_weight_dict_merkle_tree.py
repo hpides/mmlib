c91eb1b19fa9e894da75b2e046a639bf57102d7c
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2021-05-08 15:20:13 +0200

    extended testing

--- tests/util/test_weight_dict_merkle_tree.py
+++ tests/util/test_weight_dict_merkle_tree.py
@@ -3,17 +3,16 @@ import unittest
 import torch
 from torchvision.models import mobilenet_v2
 
+from tests.example_files.mynets.resnet152 import resnet152
 from util.weight_dict_merkle_tree import WeightDictMerkleTree, LEFT, LAYER_KEY, THIS, OTHER
 
-LAYER_5 = 'layer5'
-
-LAYER_4 = 'layer4'
-
-LAYER_3 = 'layer3'
-
-LAYER_2 = 'layer2'
-
 LAYER_1 = 'layer1'
+LAYER_2 = 'layer2'
+LAYER_3 = 'layer3'
+LAYER_4 = 'layer4'
+LAYER_5 = 'layer5'
+LAYER_6 = 'layer6'
+LAYER_7 = 'layer7'
 
 
 class TestWeightDictMerkleTree(unittest.TestCase):
@@ -33,8 +32,13 @@ class TestWeightDictMerkleTree(unittest.TestCase):
         self.dummy_dict2[LAYER_5] = torch.rand([10, 10])
 
         self.dummy_dict3 = self.dummy_dict.copy()
-        self.dummy_dict3['layer6'] = torch.rand([10, 10])
-        self.dummy_dict3['layer7'] = torch.rand([10, 10])
+        self.dummy_dict3[LAYER_6] = torch.rand([10, 10])
+        self.dummy_dict3[LAYER_7] = torch.rand([10, 10])
+
+        self.dummy_dict4 = self.dummy_dict.copy()
+        self.dummy_dict4[LAYER_1] = torch.rand([10, 10])
+        self.dummy_dict4[LAYER_6] = torch.rand([10, 10])
+        self.dummy_dict4[LAYER_7] = torch.rand([10, 10])
 
     def test_dummy_to_dict_to_tree(self):
         self._test_to_dict_to_tree(self.dummy_dict)
@@ -42,6 +46,11 @@ class TestWeightDictMerkleTree(unittest.TestCase):
     def test_mobilenet_to_dict_to_tree(self):
         mobilenet = mobilenet_v2(pretrained=True)
         state_dict = mobilenet.state_dict()
+        self._test_to_dict_to_tree(state_dict)
+
+    def test_mobilenet_to_dict_to_tree(self):
+        resnet = resnet152(pretrained=True)
+        state_dict = resnet.state_dict()
         self._test_to_dict_to_tree(state_dict)
 
     def _test_to_dict_to_tree(self, _dict):
@@ -65,6 +74,11 @@ class TestWeightDictMerkleTree(unittest.TestCase):
     def test_mobilenet_two_dict_same_tree(self):
         mobilenet = mobilenet_v2(pretrained=True)
         state_dict = mobilenet.state_dict()
+        self._test_two_dict_same_tree(state_dict)
+
+    def test_resnet152_two_dict_same_tree(self):
+        resnet = resnet152(pretrained=True)
+        state_dict = resnet.state_dict()
         self._test_two_dict_same_tree(state_dict)
 
     def _test_two_dict_same_tree(self, _dict):
@@ -109,9 +123,9 @@ class TestWeightDictMerkleTree(unittest.TestCase):
         tree1 = WeightDictMerkleTree(self.dummy_dict)
         tree2 = WeightDictMerkleTree(self.dummy_dict)
 
-        diff = tree1.diff_layers(tree2)
-        expected = {THIS: set(), OTHER: set()}
-        self.assertEqual(diff, expected)
+        diff_weights, diff_nodes = tree1.diff_layers(tree2)
+        self.assertEqual(diff_weights, set())
+        self.assertEqual(diff_nodes, {THIS: set(), OTHER: set()})
 
     def test_diff_same_architecture_diff_weights(self):
         tree1 = WeightDictMerkleTree(self.dummy_dict)
@@ -127,7 +141,14 @@ class TestWeightDictMerkleTree(unittest.TestCase):
 
         diff_weights, diff_nodes = tree1.diff_layers(tree2)
         self.assertEqual(diff_weights, set())
-        self.assertEqual(0, len(diff_nodes[THIS]))
-        self.assertEqual(2, len(diff_nodes[OTHER]))
+        self.assertEqual(diff_nodes[THIS], set())
+        self.assertEqual(diff_nodes[OTHER], {LAYER_6, LAYER_7})
 
+    def test_added_layers_and_diff_layers(self):
+        tree1 = WeightDictMerkleTree(self.dummy_dict)
+        tree2 = WeightDictMerkleTree(self.dummy_dict4)
 
+        diff_weights, diff_nodes = tree1.diff_layers(tree2)
+        self.assertEqual(diff_weights, {LAYER_1})
+        self.assertEqual(diff_nodes[THIS], set())
+        self.assertEqual(diff_nodes[OTHER], {LAYER_6, LAYER_7})
